425e664c07ad755c1fee5fc4da3feae452220517
Aliyah Maxine Ramos <aliyahmaxine.ramos@mail.utoronto.ca>
2023-03-02 11:29:23 -0500

    Add columns

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -13,7 +13,25 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(123)
+
+# prepare eight cups of tea: four with milk added first and the other four with milk added last
+cups <- rep(c("milk_first", "tea_first"), each = 4)
+  # rep() repeats something a certain number of times
+
+# randomize the order of all eight cups
+random_cups <- sample(cups)
+
+# create a dataset of Ian's tea groupings in four separate experiments
+experiment1 <- sample(random_cups, size = 8)
+experiment2 <- sample(random_cups, size = 8)
+experiment3 <- sample(random_cups, size = 8)
+experiment4 <- sample(random_cups, size = 8)
+tea_data <- data.frame(experiment = rep(1:4, each = 8),
+                       cup_number = 1:8,
+                       cup_type = random_cups,
+                       tester_grouping = c(experiment1, experiment2, experiment3, experiment4)) |>
+  mutate(result = ifelse(cup_type == tester_grouping, "Right", "Wrong"))
 
 
-
+tea_data
